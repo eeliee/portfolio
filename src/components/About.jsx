32208488a2ba5da8baa76dfcc6bb0da22b4cb7c3
elieguitton <elie.guitton@etudiant.univ-lr.fr>
2022-07-26 14:00:05 +0200

    Base du site

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/About.css';
+
+const About = () => {
+    return (
+        <div className="about">
+        <h1>About</h1>
+        <p>
+            This is a simple React app that I built to learn React.
+        </p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/Portfolio.css';
+
+const Portfolio = () => {
+    return (
+        <div className="portfolio">
+        <h1>Portfolio</h1>
+        <p>
+            This is a simple React app that I built to learn React.
+        </p>
+        </div>
+    );
+}
+
+export default Portfolio;
